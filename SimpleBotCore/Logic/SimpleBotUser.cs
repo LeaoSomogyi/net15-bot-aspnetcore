a4b3f5cf5a361a5a641727914a877266978a6f30
Felipe Somogyi <leaosomogyi@hotmail.com>
2019-06-05 20:18:16 -0300

    Clean code

--- SimpleBotCore/Logic/SimpleBotUser.cs
+++ SimpleBotCore/Logic/SimpleBotUser.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
+﻿
 namespace SimpleBotCore.Logic
 {
     public class SimpleBotUser
@@ -11,6 +7,5 @@ namespace SimpleBotCore.Logic
         {
             return $"{message.User} disse '{message.Text}'";
         }
-
     }
 }
