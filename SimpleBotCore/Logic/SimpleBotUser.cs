327bb1e431dbe0ea4602016f1f14b90f7ba93082
Felipe Somogyi <leaosomogyi@hotmail.com>
2019-06-05 22:55:34 -0300

    Add save and count on MongoDB

--- SimpleBotCore/Logic/SimpleBotUser.cs
+++ SimpleBotCore/Logic/SimpleBotUser.cs
@@ -1,11 +1,22 @@
-﻿
+﻿using MongoDB.Bson;
+
 namespace SimpleBotCore.Logic
 {
     public class SimpleBotUser
     {
         public string Reply(SimpleMessage message)
         {
-            return $"{message.User} disse '{message.Text}'";
+            var context = new SimpleMongoContext("FiapRobot", "messages");
+
+            context.Insert(message);
+
+            var filtro = new BsonDocument() { { "Id", message.Id } };
+
+            var count = context.CountMessages(filtro);
+
+            var _message = $"{message.User} disse '{message.Text}'({count} mensagens enviadas)";
+
+            return _message;
         }
     }
 }
---
+++ SimpleBotCore/Logic/SimpleMongoContext.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace SimpleBotCore.Logic
+{
+    public class SimpleMongoContext
+    {
+        private MongoClient MongoClient;
+        private IMongoDatabase MongoDatabase;
+        private IMongoCollection<SimpleMessage> MongoCollection;
+
+        public SimpleMongoContext(string database, string collection)
+        {
+            MongoClient = new MongoClient();
+            MongoDatabase = MongoClient.GetDatabase(database);
+            MongoCollection = MongoDatabase.GetCollection<SimpleMessage>(collection);
+        }
+
+        public void Insert(SimpleMessage document)
+        {
+            MongoCollection.InsertOne(document);
+        }
+
+        public int CountMessages(BsonDocument filter)
+        {
+            return MongoCollection.Find(filter).ToList().Count;
+        }
+    }
+}
